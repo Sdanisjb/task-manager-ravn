2d8aae37a25234c8ea888245468990235c5fbed4
sandro <sandro.caceres@ucsp.edu.pe>
2022-03-28 18:59:52 -0500

    test(MasterNavbar): created all test for functionality and structure

---
+++ src/__tests__/MasterNavBar.test.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+
+import MasterNavbar from "../components/MasterNavbar";
+
+beforeEach(() => render(<MasterNavbar />));
+
+describe("Master Navbar Mount", () => {
+  it("must include an input field of type text", () => {
+    expect(screen.getByPlaceholderText(/Search/i)).toBeInTheDocument();
+  });
+
+  it("must include a form element", () => {
+    expect(screen.getByRole("form", { name: "searchbox" })).toBeInTheDocument();
+  });
+
+  it("must include a bell and magnifying glass icon", () => {
+    expect(screen.getByLabelText(/bell icon/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/magnifying icon/i)).toBeInTheDocument();
+  });
+
+  it("must include an img tag", () => {
+    expect(screen.getByRole("img")).toBeInTheDocument();
+  });
+});
+
+describe("Delete button appeareance and functionality", () => {
+  it("on input, must appear delete icon button", () => {
+    const clearButton = screen.getByRole("button", { name: "clear button" });
+    expect(clearButton).toBeNull();
+
+    fireEvent.change(screen.getByPlaceholderText(/Search/i), {
+      target: { value: "lorem ipsum" },
+    });
+
+    expect(
+      screen.getByRole("button", { name: "clear button" })
+    ).toBeInTheDocument();
+  });
+
+  it("when clicked clear button, input target value must be null", () => {
+    const input = screen.getByPlaceholderText(/Search/i);
+    fireEvent.change(input, {
+      target: { value: "Lorem ipsum dolor sit amet." },
+    });
+
+    fireEvent.click(screen.getByRole("button", { name: "clear button" }));
+
+    expect(input.value).toBe("");
+  });
+});
