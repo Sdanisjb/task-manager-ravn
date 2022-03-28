903910827e6797ddb8abef44cf5e5f1e1d6df103
sandro <sandro.caceres@ucsp.edu.pe>
2022-03-28 12:19:22 -0500

    fix (SideBarItem): Removed warnings for non-existent roles

--- src/components/SideBarItem.js
+++ src/components/SideBarItem.js
@@ -1,17 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import { RiFunctionLine, RiMenuFill } from "react-icons/ri";
 
 const SideBarItem = ({ icon, labelText }) => {
   return (
-    <div role="sidebar-container">
-      {icon === "dashboard" && (
-        <RiFunctionLine role="icon" aria-label="sidebar-icon" />
-      )}
-      {icon === "tasks" && <RiMenuFill role="icon" aria-label="sidebar-icon" />}
+    <div aria-label="sidebar-container">
+      {icon === "dashboard" && <RiFunctionLine aria-label="sidebar-icon" />}
+      {icon === "tasks" && <RiMenuFill aria-label="sidebar-icon" />}
 
-      <label role="label" aria-label="sidebar-label">
-        {labelText}
-      </label>
+      <label aria-label="sidebar-label">{labelText}</label>
     </div>
   );
 };
