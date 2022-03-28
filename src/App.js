0af5701d37aeb5c5bfd2962947bcc3e78fee1317
sandro <sandro.caceres@ucsp.edu.pe>
2022-03-28 11:44:31 -0500

    chore (App, index) remove boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SideBarItem from "./components/SideBarItem";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SideBarItem icon="dashboard" labelText="dashboard" />
     </div>
   );
 }
