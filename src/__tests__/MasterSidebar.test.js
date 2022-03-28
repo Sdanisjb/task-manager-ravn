932bb2b43ac4361d00f3450a3e679dffec01f096
sandro <sandro.caceres@ucsp.edu.pe>
2022-03-28 17:43:48 -0500

    test(MasterSideBar): structural test implemented

---
+++ src/__tests__/MasterSidebar.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import MasterSidebar from "../components/MasterSidebar";
+
+beforeEach(() => render(<MasterSidebar />));
+
+describe("Master Sidebar Mount", () => {
+  it("must show two items", () => {
+    expect(screen.getAllByLabelText("sidebaritem-container").length).toBe(2);
+  });
+
+  it("must show a 'dashboard' tag", () => {
+    expect(screen.getByText("dashboard")).toBeInTheDocument();
+  });
+
+  it("must show a 'My Tasks' tag", () => {
+    expect(screen.getByText("my tasks")).toBeInTheDocument();
+  });
+});
+
+describe("check good practices", () => {
+  it("must use a nav element", () => {
+    expect(screen.getByRole("navigation")).toBeInTheDocument();
+  });
+});
